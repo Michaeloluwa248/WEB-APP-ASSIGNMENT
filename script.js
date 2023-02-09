02e2b3ea6c0b2db9ecd6d812af259d7123b4dc36
John Alex <realfinebobo@gmail.com>
2023-02-09 16:41:29 +0100

    Added Back to top button to all pages

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
   $(document).ready(function (){
     document.querySelector('body').classList.add("loaded")  
     console.log("Welcome to michael school of language");
+     /* begin begin Back to Top button  */
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back_to_top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 10) {
+      window.scrollBy(0, -1000);
+      setTimeout(backToTop, 200);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.back_to_top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
+/* end begin Back to Top button  */
   })
+
+ 
